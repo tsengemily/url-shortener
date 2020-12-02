68bf8fae93a3ba2d246402b5779dcc34739421d3
Emily <tsengemily818@gmail.com>
2020-12-02 11:31:44 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express')
 const exphbs = require('express-handlebars')
 const bodyParser = require('body-parser')
+require('./config/mongoose')
 
 const app = express()
 const port = 3000
