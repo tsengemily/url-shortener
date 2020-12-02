ef647d1e4fe77f16d8180bcba8c44bf712165f49
Emily <tsengemily818@gmail.com>
2020-12-02 11:08:25 +0800

    feat: project init

---
+++ app.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+
+const app = express()
+const port = 3000
+
+app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
+
+app.use(bodyParser.urlencoded({ extended: true }))
+
+app.get('/', (req, res) => {
+  res.send('hello')
+})
+
+app.listen(port, () => {
+  console.log(`App is listening on http://localhost:${port}`)
+})
